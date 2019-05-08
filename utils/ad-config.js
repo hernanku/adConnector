87248b5400c949901bf6a395dcd56bb200c761ef
Hernandez Aku <43397610+hernanku@users.noreply.github.com>
2019-05-07 19:03:04 -0500

    Removing unneccesary data from code

--- utils/ad-config.js
+++ utils/ad-config.js
@@ -13,7 +13,7 @@
 module.exports = {
     baseUrl: 'http://localhost:3000',
     repUrl: 'http://localhost:8000',
-    //addictUrl: 'addict --url ldaps://10.11.25.20:636 --user pocsvcacc2@vmwarepoc.com --pass Passw0rd2', //test
+    //addictUrl: 'addict --url ldaps://[address]:[port] --user [user]@[domain] --pass [pass]', //test
     addictUrl : 'addict --url ldaps://[address]:[port] --user [user]@[domain] --pass [pass],//live
     //repAdmin: 'repadmin /syncall /AdeP [address] dc=[domain],dc=[domain_suffix] /u:[domain]\\[user] /pw:[pass]',
     repAdmin: 'repadmin /syncall /AdeP [address] dc=[domain],dc=[domain_suffix] /u:[domain]\\[user] /pw:[pass]',
